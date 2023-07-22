fa2917138ff1813f7d881e6c9c8c2a822c5bdba5
jjkip <jjlangatjose@gmail.com>
2023-07-22 23:52:20 +0300

    Initial Commit

---
+++ FloatingButton/FloatingButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingButtonApp.swift
+//  FloatingButton
+//
+//  Created by Joseph Langat on 22/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct FloatingButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
